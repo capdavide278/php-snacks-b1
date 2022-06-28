3741c174888d56cbe67f799e8a2a07982915f362
Davide <franc.davi00@gmail.com>
2022-06-28 10:15:44 +0200

    inserimento controllo per vedere se i parametri iniziali sono settati

--- j-snacks_2/index.php
+++ j-snacks_2/index.php
@@ -3,9 +3,9 @@
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
 che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”*/
 
-    $name = $_GET['name'];
-    $mail = $_GET['mail'];
-    $age = $_GET['age'];
+    $name = isset($_GET['name']) ? $_GET['name'] : '';
+    $mail = isset($_GET['mail']) ? $_GET['mail'] : '';
+    $age = $_GET['age'] ?? '';//scorciatoia
 
 ?>
 
@@ -29,7 +29,7 @@ che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è
 
     <?php 
 
-        if (strlen($name) >= 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
+        if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
             echo 'accesso riuscito';
         } else {
             echo 'accesso negato';
